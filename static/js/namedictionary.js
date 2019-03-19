6d45da8be6b800e2f36e8e45664996af2172dd64
Unknown <ni3fidzialny@gmail.com>
2019-03-19 20:09:57 +0100

    Tooltip fixes

---
+++ static/js/namedictionary.js
@@ -0,0 +1,8 @@
+const names = [
+    'Technikum Gospodarki Komunalnej',
+    'Technikum Chemiczne',
+    'Technikum Mechaniczno-Energetyczne',
+    'Zespół Szkół Mechaniczno-Energetycznych',
+    'Lubelskie Centrum Edukacji Zawodowej',
+    'Lubelskie Centrum Kształcenia Zawodowego i Ustawicznego'
+];
